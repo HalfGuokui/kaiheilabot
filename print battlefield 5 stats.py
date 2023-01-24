b3692fe213077f131f260982367a4d06b1310947
HalfGuokui <105647442+HalfGuokui@users.noreply.github.com>
2023-01-24 13:47:16 +0800

    Update print battlefield 5 stats.py

--- print battlefield 5 stats.py
+++ print battlefield 5 stats.py
@@ -1,20 +1,22 @@
 import json
 import requests
 
-playername = input('请输入你的origin ID：')
+playername = input('请输入你的Origin ID：')
 
 url = f'https://api.gametools.network/bfv/stats/?format_values=true&name={playername}&platform=pc&skip_battlelog=false&lang=zh-CN'
 
 r= requests.get(url)
-data = json.loads(r.text)
-
+print("状态码：",r.status_code)#返回api状态码
+#print(f"返回状态码：{r.status_code}")#返回api状态码第二种写法
 #print(data['userName'], data['rank'], data['id'])
 
-#print(f"返回状态码：{r.status_code}")#返回api状态码
 respones_dict = r.json()
+
 #print(respones_dict.keys())#打印键值
+
 #print(f"头像图片文件：{respones_dict['avatar']}")
-print(f"Origin ID：{respones_dict['userName']}")
+print(f"玩家昵称：{respones_dict['userName']}")
+#print("玩家昵称：", respones_dict['userName'])
 print(f"游戏时长：{respones_dict['timePlayed']}")
 print(f"等级：{respones_dict['rank']}")
 #print(f"等级图片文件：{respones_dict['rankImg']}")
@@ -33,5 +35,6 @@ print(f"最远爆头距离：{respones_dict['longestHeadShot']}米")
 #print(f"已完成对局：{respones_dict['roundsPlayed']}")
 print(f"胜场：{respones_dict['wins']}")
 print(f"负场：{respones_dict['loses']}")
-print(f"胜率：{respones_dict['wins']}")
+print(f"胜率：{respones_dict['winPercent']}")
 print(f"最佳兵种：{respones_dict['bestClass']}")
+
